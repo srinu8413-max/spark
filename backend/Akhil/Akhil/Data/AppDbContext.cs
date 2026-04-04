a1f142bd123fdf261a10ae127238c58d2e34d50b
Akhil70217021 <132738612+Akhil70217021@users.noreply.github.com>
2026-04-04 18:06:16 +0530

    Delete backend directory

--- backend/Akhil/Akhil/Data/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-public class AppDbContext : DbContext
-{
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    public DbSet<User> Users { get; set; }
-    public DbSet<Queue> Queues { get; set; }
-}
--- backend/Akhil/Akhil/Models/Queue.cs
+++
@@ -1,8 +0,0 @@
-﻿public class Queue
-{
-    public int Id { get; set; }
-    public int UserId { get; set; }
-    public DateTime JoinedAt { get; set; }
-
-    public User User { get; set; }
-}
