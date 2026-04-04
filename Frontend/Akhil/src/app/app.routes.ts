f0b91713a17d5dfe696d526f72ba622e1328df88
Akhil70217021 <132738612+Akhil70217021@users.noreply.github.com>
2026-04-04 18:06:02 +0530

    Delete Frontend/Akhil directory

--- Frontend/Akhil/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { RegisterComponent } from './register/register';
-import { QueueComponent } from './queue/queue';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'register', pathMatch: 'full' as const },
-  { path: 'register', component: RegisterComponent },
-  { path: 'queue', component: QueueComponent }
-];
